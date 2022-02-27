769c4d05192b0616d313fda758172b8fbcfb0f10
cpusch <conpusch@yahoo.com>
2022-02-27 19:22:40 +0000

    Implemented emtpy arrays tests for all algorithms

--- SortComparison/src/SortComparisonTest.java
+++ SortComparison/src/SortComparisonTest.java
@@ -6,46 +6,66 @@ import org.junit.runners.JUnit4;
 
 //-------------------------------------------------------------------------
 /**
- *  Test class for SortComparison.java
+ * Test class for SortComparison.java
  *
- *  @author
- *  @version HT 2020
+ * @author
+ * @version HT 2020
  */
 @RunWith(JUnit4.class)
-public class SortComparisonTest
-{
-    //~ Constructor ........................................................
+public class SortComparisonTest {
+    // ~ Constructor ........................................................
     @Test
-    public void testConstructor()
-    {
+    public void testConstructor() {
         new SortComparison();
     }
 
-    //~ Public Methods ........................................................
+    // ~ Public Methods ........................................................
 
     // ----------------------------------------------------------
     /**
      * Check that the methods work for empty arrays
      */
     @Test
-    public void testEmpty()
-    {
+    public void testEmpty() {
+
+        double a[] = {};
+        double sortedArray[] = new double[a.length];
+        int length;
+
+        sortedArray = SortComparison.insertionSort(a);
+        length = sortedArray.length;
+        assertEquals("Checking insertion Empty", length, 0);
+
+        sortedArray = SortComparison.quickSort(a);
+        length = sortedArray.length;
+        assertEquals("CHecking Quick Empty", length, 0);
+
+        sortedArray = SortComparison.mergeSortIterative(a);
+        length = sortedArray.length;
+        assertEquals("Checking merge Iterative empty", length, 0);
+
+        sortedArray = SortComparison.mergeSortRecursive(a);
+        length = sortedArray.length;
+        assertEquals("CHecking merge Recusive empty", length, 0);
+
+        sortedArray = SortComparison.selectionSort(a);
+        length = sortedArray.length;
+        assertEquals("Checking Selection Empty", length, 0);
     }
 
-
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // TODO: add more tests here. Each line of code and ech decision in
+    // Collinear.java should
     // be executed at least once from at least one test.
 
     // ----------------------------------------------------------
     /**
-     *  Main Method.
-     *  Use this main method to create the experiments needed to answer the experimental performance questions of this assignment.
+     * Main Method.
+     * Use this main method to create the experiments needed to answer the
+     * experimental performance questions of this assignment.
      *
      */
-    public static void main(String[] args)
-    {
-        //TODO: implement this method
+    public static void main(String[] args) {
+        // TODO: implement this method
     }
 
 }
-
