01517257106345433e1f32f34560c92065b68631
cpusch <conpusch@yahoo.com>
2022-03-31 16:27:46 +0100

    removed main and updated graph to use only matrix

--- ShortestPath/src/CompetitionFloydWarshall.java
+++ ShortestPath/src/CompetitionFloydWarshall.java
@@ -6,18 +6,10 @@ public class CompetitionFloydWarshall {
     private int sA, sB, sC;
     private int slowestWalker;
 
-    public static void main(String args[]) {
-        String file = "/home/user/Semester2/Algorithms-II/ShortestPath/src/inputAssignment2/input-I.txt";
-        CompetitionFloydWarshall test = new CompetitionFloydWarshall(file, 60, 70, 80);
-        System.out.println(test.timeRequiredforCompetition());
-
-    }
-
     // class that hanldles the graph implementation using an
     // adjacency list. Also parses file to generate graph
     static class Graph {
-        private List<List<Node>> adj_list = new ArrayList<>();
-        private double[][] adj_matrix = new double[numNodes][numNodes];
+        private double[][] adjMatrix;
         private static int numEdges;
         private static int numNodes;
 
@@ -26,28 +18,7 @@ public class CompetitionFloydWarshall {
          * @param edges list of edges to then create the graph
          */
         Graph(String filename) {
-            List<Edge> edges = parseFile(filename);
-            // adjacency list memory allocation
-            for (int i = 0; i < numNodes; i++)
-                adj_list.add(i, new ArrayList<>());
-
-            // add edges to the graph
-            for (Edge e : edges) {
-                // allocate new node in adjacency List from src to dest
-                adj_list.get(e.src).add(new Node(e.dest, e.weight));
-            }
-            double temp[][] = new double[numNodes][numNodes];
-            for (int i = 0; i < numNodes; i++) {
-                for (int j = 0; j < numNodes; j++)
-                    temp[i][j] = Double.POSITIVE_INFINITY;
-            }
-
-            for (int i = 0; i < numNodes; i++) {
-                for (Node n : adj_list.get(i)) {
-                    temp[i][n.value] = n.weight;
-                }
-            }
-            adj_matrix = temp;
+            adjMatrix = parseFile(filename);
         }
 
         int getNumEdges() {
@@ -58,25 +29,26 @@ public class CompetitionFloydWarshall {
             return numNodes;
         }
 
-        List<List<Node>> getGraph() {
-            return adj_list;
-        }
-
         double[][] getMatrix() {
-            return adj_matrix;
+            return adjMatrix;
         }
 
-        private static List<Edge> parseFile(String filename) {
-            List<Edge> edges = new ArrayList<Edge>();
+        private static double[][] parseFile(String filename) {
+            double temp[][] = null;
             try {
                 File file = new File(filename);
                 Scanner input = new Scanner(file);
                 // skip the first two lines and set appropriat values
                 numNodes = input.nextInt();
                 numEdges = input.nextInt();
+                temp = new double[numNodes][numNodes];
+                for (int i = 0; i < numNodes; i++) {
+                    for (int j = 0; j < numNodes; j++)
+                        temp[i][j] = Double.POSITIVE_INFINITY;
+                }
                 // adds edges from file to edge list
                 while (input.hasNextLine()) {
-                    edges.add(new Edge(input.nextInt(), input.nextInt(), input.nextDouble()));
+                    temp[input.nextInt()][input.nextInt()] = input.nextDouble();
                 }
                 input.close();
             } catch (Exception e) {
@@ -84,37 +56,7 @@ public class CompetitionFloydWarshall {
                 e.printStackTrace();
             }
 
-            return edges;
-        }
-    }
-
-    // represents edges in graph
-    private static class Edge {
-        int src, dest;
-        double weight;
-
-        /**
-         * 
-         * @param src    source node
-         * @param dest   destination node
-         * @param weight weight of edge
-         */
-        Edge(int src, int dest, double weight) {
-            this.src = src;
-            this.dest = dest;
-            this.weight = weight;
-        }
-
-    }
-
-    // represents nodes in grpah
-    private static class Node {
-        int value;
-        double weight;
-
-        Node(int value, double weight) {
-            this.value = value;
-            this.weight = weight;
+            return temp;
         }
     }
 
