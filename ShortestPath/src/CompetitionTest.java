ab43741d00b2cbebb3cfbb93d25ed73dd662c49c
cpusch <conpusch@yahoo.com>
2022-03-31 18:27:00 +0100

    removed not used imports

--- ShortestPath/src/CompetitionTest.java
+++ ShortestPath/src/CompetitionTest.java
@@ -1,14 +1,8 @@
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Arrays;
-import java.util.Scanner;
 
 @RunWith(JUnit4.class)
 public class CompetitionTest {
