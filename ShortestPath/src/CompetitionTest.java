19e108ed125b24282a39b0c8136b4c449e952be5
cpusch <conpusch@yahoo.com>
2022-03-31 16:28:48 +0100

    added more tests for larger code coverage

--- ShortestPath/src/CompetitionTest.java
+++ ShortestPath/src/CompetitionTest.java
@@ -11,20 +11,64 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 @RunWith(JUnit4.class)
-public class CompetitionTest{
+public class CompetitionTest {
+    String file = "/home/user/Semester2/Algorithms-II/ShortestPath/src/input-K.txt";
+    String file2 = "/home/user/Semester2/Algorithms-II/ShortestPath/src/inputAssignment2/input-J.txt";
+    String file3 = "/home/user/Semester2/Algorithms-II/ShortestPath/src/inputAssignment2/input-F.txt";
 
     @Test
     public void testDijkstraConstructor() {
 
-        //TODO
+        new CompetitionDijkstra(file, 50, 60, 70);
     }
 
     @Test
     public void testFWConstructor() {
-        //TODO
+        new CompetitionFloydWarshall(file, 50, 60, 70);
     }
 
-    //TODO - more tests
-    
-}
+    @Test
+    public void testDijkstraTime() {
+        CompetitionDijkstra test = new CompetitionDijkstra(file, 50, 60, 70);
+        int speed = test.timeRequiredforCompetition();
+        assertEquals(320, speed);
 
+        test = new CompetitionDijkstra(file2, 50, 60, 70);
+        speed = test.timeRequiredforCompetition();
+        assertEquals(-1, -1);
+    }
+
+    @Test
+    public void testWarshallTime() {
+        CompetitionFloydWarshall test = new CompetitionFloydWarshall(file, 50, 60, 70);
+        int speed = test.timeRequiredforCompetition();
+        assertEquals(320, speed);
+
+        test = new CompetitionFloydWarshall(file2, 50, 60, 70);
+        speed = test.timeRequiredforCompetition();
+        assertEquals(-1, -1);
+    }
+
+    @Test
+    public void testFalseParameters() {
+        CompetitionDijkstra dijk = new CompetitionDijkstra(file, 40, 50, 60);
+        int speed = dijk.timeRequiredforCompetition();
+        assertEquals(-1, speed);
+
+        CompetitionFloydWarshall floyd = new CompetitionFloydWarshall(file, 40, 60, 70);
+        speed = floyd.timeRequiredforCompetition();
+        assertEquals(-1, speed);
+    }
+
+    @Test
+    public void testFalseGraph() {
+        CompetitionDijkstra dijk = new CompetitionDijkstra(file3, 50, 80, 60);
+        int speed = dijk.timeRequiredforCompetition();
+        assertEquals(-1, speed);
+
+        CompetitionFloydWarshall floyd = new CompetitionFloydWarshall(file3, 50, 60, 70);
+        speed = floyd.timeRequiredforCompetition();
+        assertEquals(-1, speed);
+    }
+
+}
